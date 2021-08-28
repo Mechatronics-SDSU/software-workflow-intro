e2a3cb1bb632040d1c375f0b11e01fd5695ddd57
Anthony NorderhendqCDcd <anthony@norderhaug.org>
2021-08-28 13:32:08 -0700

    put my info & renamed template

---
+++ submissions/anorderhaug.py
@@ -0,0 +1,24 @@
+"""Introduce yourself to the Mechatronics team by coding in Python!
+***USE GIT TO MAKE A NEW BRANCH***
+Please refer to our git guide for how to do this. The branch should be called `submission-[your name]`,
+where [your name] should be your first initial of your first name and then your last name, all lowercase.
+***RENAME THIS FILE***
+This file's name should be your first initial of your first name and then your last name, all lowercase.
+
+Goal:
+Modify this file to print out the following:
+1. Your name, year, major
+2. A hobby you like and something interesting about yourself
+3. What your goals are for Mechatronics
+
+After you've tested and verified your code works, push it to the remote repository on a new branch.
+
+***IMPORTANT***
+To finish this task, you need to now push it to GitHub.
+After you push the code to your own branch, on the GitHub, go `[n] branches`, find your branch, and submit
+a Pull Request to the "introduction-submission" branch. Once that's been approved you're done! congrats!
+"""
+
+print('Anthony Norderhaug, Junior, Computer Science')
+print('I like filming and editing videos and I love Quest protein bars.')
+print('I would like to learn how to program computer vision.')
