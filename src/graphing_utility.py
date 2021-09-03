f08911821b9ba458b5c24d150823b567a27117d1
David Saifer <spyrotheultimate@gmail.com>
2021-09-02 21:28:23 -0700

    Graphing utility possible final commit

--- src/graphing_utility.py
+++ src/graphing_utility.py
@@ -1,57 +1,74 @@
+#python
 from datetime import datetime as dt
 import datetime
 import random
 import json
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 import copy
 
-# Can add speed and live reading fairly easily
+#external libs
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
-##########
+def main():
+    """Driver code for the graphing utility
 
-begin = 5
-end = 20
+    :return: None
+    """
+    global begin
+    global end
+    global data_start
+    global data
+    global ax1
+    global graphing_start
+    ##########
 
-##########
+    begin = 0
+    end = 20
 
-plt.style.use("fivethirtyeight")
-fig = plt.figure()
-ax1 = fig.add_subplot(1, 1, 1)
-ax1.set_xlim(begin)
+    ##########
 
-data = []
+    plt.style.use("fivethirtyeight")
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1, 1, 1)
+    ax1.set_xlim(begin)
 
-file_name = input("Enter Json file, type \"test\" for test data, or \"testjson\" to make a json file to read from ")
 
-if file_name == "test" or file_name == "testjson":
-    for inc in range(100):
-        data.append([(dt.utcnow() + datetime.timedelta(seconds=inc)), random.randint(1, 100)])
+    data = []
 
-# Write data to json file
-if file_name == "testjson":
-    write_data = copy.deepcopy(data)
-    with open("testJSON.json", 'w') as file:
-        file.truncate(0)
-        inc = 0
-        while inc <= len(data) - 1:
-            write_data[inc][0] = write_data[inc][0].strftime('%Y-%m-%dT%H:%M:%S.%f')
-            inc += 1
+    file_name = input("Enter Json file, type \"test\" for test data, or \"testjson\" to make a json file to read from ")
 
-        json.dump(write_data, file, indent=True)
-    file_name = "testJSON.json"
+    if file_name == "test" or file_name == "testjson":
+        for inc in range(100):
+            data.append([(dt.utcnow() + datetime.timedelta(seconds=inc)), random.randint(1, 100)])
 
-# Open Json file and read in utc as string
-if file_name != "test":
-    with open(file_name, "r") as file:
-        data = json.load(file)
-        inc = 0
-        while inc <= len(data) - 1:
-            data[inc][0] = dt.strptime(data[inc][0], '%Y-%m-%dT%H:%M:%S.%f')
-            inc += 1
+    # Write data to json file
+    if file_name == "testjson":
+        write_data = copy.deepcopy(data)
+        with open("testJSON.json", 'w') as file:
+            file.truncate(0)
+            inc = 0
+            while inc <= len(data) - 1:
+                write_data[inc][0] = write_data[inc][0].strftime('%Y-%m-%dT%H:%M:%S.%f')
+                inc += 1
 
-data_start = data[0][0]
-graphing_start = dt.utcnow()
+            json.dump(write_data, file, indent=True)
+        file_name = "testJSON.json"
+
+    # Open Json file and read in utc as string
+    if file_name != "test":
+        with open(file_name, "r") as file:
+            data = json.load(file)
+            inc = 0
+            while inc <= len(data) - 1:
+                data[inc][0] = dt.strptime(data[inc][0], '%Y-%m-%dT%H:%M:%S.%f')
+                inc += 1
+
+    data_start = data[0][0]
+    graphing_start = dt.utcnow()
+
+    ani = animation.FuncAnimation(fig, animate, interval=100)
+
+    plt.show()
 
 
 def animate(i) -> None:
@@ -80,6 +97,6 @@ def animate(i) -> None:
     ax1.plot(plot_x, plot_y)
 
 
-ani = animation.FuncAnimation(fig, animate, interval=100)
 
-plt.show()
+if __name__ == "__main__":
+    main()
