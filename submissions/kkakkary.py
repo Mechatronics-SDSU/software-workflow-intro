45f0902d6eeb8b21e0d229d07b78db15c34f4464
kkakkary <113316020+kkakkary@users.noreply.github.com>
2022-09-17 12:01:40 -0700

    Update kkakkary.py

--- submissions/kkakkary.py
+++ submissions/kkakkary.py
@@ -1,3 +1,3 @@
-print('Kevin Kakkary, Junior, Computer Engineering")
+print('Kevin Kakkary, Junior, Computer Engineering')
 print('I like piano and calligraphy')
 print('I would like to learn more about mechatronics.')
