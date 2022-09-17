ec5d2506875abc971f4c50f607f27656b05f8264
Aleva222 <112987273+Aleva222@users.noreply.github.com>
2022-09-17 12:22:53 -0700

    Update and rename template.py to avaqueranava.py

---
+++ src/submissions/avaqueranava.py
@@ -0,0 +1,3 @@
+print('Alejandro Vaquera Nava, Junior, Aerospace Engineer')
+print('I enjoy practicing piano, guitar, and I enjoy excercising')
+print('I would like to learn more about other programming languages')
