c02c19b6e42ac3774c2227586e0a5a61e65a23e7
Panstan <86463601+Panstan@users.noreply.github.com>
2023-02-14 17:47:15 -0800

    Update publisher.py

--- IntroWorkSpaceTalor/src/timepublisher/timepublisher/publisher.py
+++ IntroWorkSpaceTalor/src/timepublisher/timepublisher/publisher.py
@@ -10,11 +10,12 @@ class MinimalService(Node):
     def __init__(self):
         super().__init__('minimal_service')
         self.srv = self.create_service(Humantime, 'UnixToHuman', self.UnixToHuman)
-
+    
     def UnixToHuman(self, request, response):
+        #get request from client, then convert sent Unix time into human readable time eg. 2023-02-14 17:45:55.
         response.humantime = datetime.datetime.utcfromtimestamp(request.unixtime).strftime('%Y-%m-%d %H:%M:%S')
         self.get_logger().info('Incoming request\nUnixtime: %d' % (request.unixtime))
-
+        #then send the converted time back to client
         return response
 
 
