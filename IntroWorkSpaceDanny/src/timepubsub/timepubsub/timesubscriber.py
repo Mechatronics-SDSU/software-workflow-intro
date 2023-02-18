feaae61f3afe46e62f618f97dcd05fffdfeaba70
Danny <69557137+JiaoziDani@users.noreply.github.com>
2023-02-18 13:03:09 -0800

    Add files via upload

--- IntroWorkSpaceDanny/src/timepubsub/timepubsub/timesubscriber.py
+++ IntroWorkSpaceDanny/src/timepubsub/timepubsub/timesubscriber.py
@@ -1,23 +1,35 @@
 import rclpy
 from rclpy.node import Node
-
 from std_msgs.msg import String
+import time
 
 
-class MinimalSubscriber(Node):
-
-    def __init__(self):
+class MinimalSubscriber(Node):  
+    def __init__(self) -> None:
         super().__init__('timesubscriber')
         self.subscription = self.create_subscription(
             String,
-            'topic',
+            'epoch_time',
             self.listener_callback,
             10)
-        self.subscription  # prevent unused variable warning
+        
+        self.publisher = self.create_publisher(
+            String,
+            'date_time',
+            10)
+            
+    def listener_callback(self, msg)-> None:
+        self.get_logger().info('Unix Epoch Time: "%s"' % msg.data)
+        reply = String()
+        reply.data = self.time_converter(msg.data)
+        self.publisher.publish(reply)
 
-    def listener_callback(self, msg):
-        self.get_logger().info('Real Date Time: "%s"' % msg.data)
-
+    def time_converter(self, unix_time) -> str:
+    
+        real_time = time.localtime(float(unix_time))
+        actual_time = time.strftime('%Y-%m-%d %H:%M:%S', real_time)
+        
+        return actual_time
 
 def main(args=None):
     rclpy.init(args=args)
