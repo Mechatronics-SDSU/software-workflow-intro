f85bcea01bb0ea7b9653b1e6be6ce2e77706e183
ayuyamo <118097704+ayuyamo@users.noreply.github.com>
2023-02-18 12:45:07 -0800

    Update project_publisher.py

--- project_publisher.py
+++ project_publisher.py
@@ -1,34 +1,41 @@
 import rclpy
-from rclpy.node import Node
-from std_msgs.msg import Int64, String
-import datetime
+import time
 
-class UnixEpochTimeSubscriber(Node):
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+
+class UnixEpochTimePublisher(Node):
 
     def __init__(self):
-        super().__init__('unix_epoch_time_subscriber')
-        self.subscription = self.create_subscription(
-            Int64,
-            'unix_epoch_time',
-            self.callback,
-            10)
-        self.publisher_ = self.create_publisher(String, 'human_readable_date', 10)
+        super().__init__('unix_epoch_time_publisher')
+        self.publisher_ = self.create_publisher(String, 'unix_epoch_time_conversion', 10)
+        timer_period = 5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
 
-    def callback(self, msg):
-        # Convert the unix epoch time to a human-readable date
-        date = datetime.datetime.fromtimestamp(msg.data).strftime('%Y-%m-%d %H:%M:%S')
-        # Publish the human-readable date
+    def timer_callback(self):
         msg = String()
-        msg.data = date
+        msg.data = str(int(time.time()))
         self.publisher_.publish(msg)
-        self.get_logger().info('Converted unix epoch time to human-readable date: %s' % date)
+        self.get_logger().info('unix epoch time: "%s"' % msg.data)
+        self.i += 1
+
 
 def main(args=None):
     rclpy.init(args=args)
-    unix_epoch_time_subscriber = UnixEpochTimeSubscriber()
-    rclpy.spin(unix_epoch_time_subscriber)
-    unix_epoch_time_subscriber.destroy_node()
+
+    unix_epoch_time_publisher = UnixEpochTimePublisher()
+
+    rclpy.spin(unix_epoch_time_publisher)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    unix_epoch_time_publisher.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
